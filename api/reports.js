779b597ec13dc8deb8dcccc3db117ea44b29d7ca
a56932793-cmd <a56932793@gmail.com>
2026-07-31 15:55:39 +0900

    Add api/reports.js

---
+++ api/reports.js
@@ -0,0 +1,6 @@
+import { json, supabaseGet } from './_supabase.js';
+export default async function handler(_req, res) {
+  try { json(res, await supabaseGet('analysis_reports?select=*&order=created_at.desc')); }
+  catch (error) { json(res, { error: error.message }, 500); }
+}
+
